669bd72117d37c694dc800860d02bb7f14244f7a
Ruben <megarex333@gmail.com>
2021-04-04 19:34:47 +0200

    Hover info

--- src/App.vue
+++ src/App.vue
@@ -1,144 +1,136 @@
 <template>
-  <div class="container">
-    <c-box d="flex" w="100vw" h="100vh" flex-dir="column">
-      <Navbar />
-      <c-flex justify="center" direction="column" align="center">
-        <Map :data="data" />
-        <c-box
-          :width="[
-            '100%', // base
-            '50%', // 480px upwards
-            '70%', // 768px upwards
-            '40%', // 992px upwards
-          ]"
-        >
-          <c-button-group>
-            <c-button
-              variant-color="green"
-              @click="
-                {
-                  if (pendingData.length > 0) data.push(pendingData.pop());
-                }
-              "
-              >Add</c-button
-            >
-            <c-button
-              variant-color="red"
-              @click="
-                {
-                  if (data.length > 0) pendingData.push(data.pop());
-                }
-              "
-              >Remove</c-button
-            >
-          </c-button-group>
-          <c-stat-group>
-            <Stat
-              gas="CO2"
-              unit="ppm"
-              :avg="avgGas('co2')"
-              :ok="1000"
-              :high="2000"
-            />
-            <Stat gas="O3" unit="ppb" :avg="avgGas('o3')" :ok="55" :high="61" />
-            <Stat
-              gas="NO2"
-              unit="ppb"
-              :avg="avgGas('no2')"
-              :ok="50"
-              :high="100"
-            />
-            <Stat
-              gas="SO2"
-              unit="ppb"
-              :avg="avgGas('so2')"
-              :ok="100"
-              :high="200"
-            />
-          </c-stat-group>
-        </c-box>
-      </c-flex>
-    </c-box>
-  </div>
+	<div class="container">
+		<c-box d="flex" w="100vw" h="100vh" flex-dir="column">
+			<Navbar />
+			<c-flex justify="center" direction="column" align="center">
+				<Map :data="data" />
+				<c-box
+					:width="[
+						'100%', // base
+						'50%', // 480px upwards
+						'70%', // 768px upwards
+						'40%' // 992px upwards
+					]"
+				>
+					<c-button-group>
+						<c-button
+							variant-color="green"
+							@click="
+								{
+									if (pendingData.length > 0)
+										data.push(pendingData.pop());
+								}
+							"
+							>Add</c-button
+						>
+						<c-button
+							variant-color="red"
+							@click="
+								{
+									if (data.length > 0)
+										pendingData.push(data.pop());
+								}
+							"
+							>Remove</c-button
+						>
+					</c-button-group>
+					<c-stat-group>
+						<Stat
+							gas="CO2"
+							unit="ppm"
+							:avg="avgGas('co2')"
+							:ok="1000"
+							:high="2000"
+						/>
+						<Stat
+							gas="O3"
+							unit="ppb"
+							:avg="avgGas('o3')"
+							:ok="55"
+							:high="61"
+						/>
+						<Stat
+							gas="NO2"
+							unit="ppb"
+							:avg="avgGas('no2')"
+							:ok="50"
+							:high="100"
+						/>
+						<Stat
+							gas="SO2"
+							unit="ppb"
+							:avg="avgGas('so2')"
+							:ok="100"
+							:high="200"
+						/>
+					</c-stat-group>
+				</c-box>
+			</c-flex>
+		</c-box>
+	</div>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
-import Map from "@/components/Map";
-import Stat from "@/components/Stat";
+	import Navbar from '@/components/Navbar';
+	import Map from '@/components/Map';
+	import Stat from '@/components/Stat';
 
-export default {
-  name: "App",
-  data: () => ({
-    data: [],
-    pendingData: [
-      {
-        gas: {
-          co2: Math.random() * 2500 + 200,
-          o3: Math.random() * 70 + 30,
-          no2: Math.random() * 110 + 40,
-          so2: Math.random() * 220 + 90,
-        },
-        coordinates: [-4.45021390914917, 36.71963561245719],
-      },
-      {
-        gas: {
-          co2: Math.random() * 2500 + 200,
-          o3: Math.random() * 70 + 30,
-          no2: Math.random() * 110 + 40,
-          so2: Math.random() * 220 + 90,
-        },
-        coordinates: [-4.431438446044922, 36.73240113505636],
-      },
-      {
-        gas: {
-          co2: Math.random() * 2500 + 200,
-          o3: Math.random() * 70 + 30,
-          no2: Math.random() * 110 + 40,
-          so2: Math.random() * 220 + 90,
-        },
-        coordinates: [-4.478076696395874, 36.715215125584294],
-      },
-      {
-        gas: {
-          co2: Math.random() * 2500 + 200,
-          o3: Math.random() * 70 + 30,
-          no2: Math.random() * 110 + 40,
-          so2: Math.random() * 220 + 90,
-        },
-        coordinates: [-4.490876197814941, 36.67908918792989],
-      },
-      {
-        gas: {
-          co2: Math.random() * 2500 + 200,
-          o3: Math.random() * 70 + 30,
-          no2: Math.random() * 110 + 40,
-          so2: Math.random() * 220 + 90,
-        },
-        coordinates: [-4.486284255981445, 36.74129146113483],
-      },
-      {
-        gas: {
-          co2: Math.random() * 2500 + 200,
-          o3: Math.random() * 70 + 30,
-          no2: Math.random() * 110 + 20,
-          so2: Math.random() * 220 + 40,
-        },
-        coordinates: [-4.415860176086426, 36.749613386425054],
-      },
-    ],
-  }),
-  methods: {
-    avgGas(gas) {
-      return Math.round(
-        this.data.reduce((p, c) => p + c.gas[gas], 0) / this.data.length
-      );
-    },
-  },
-  components: {
-    Navbar,
-    Map,
-    Stat,
-  },
-};
+	export default {
+		name: 'App',
+		data: () => ({
+			data: []
+		}),
+		mounted() {
+			this.pendingData = [
+				{
+					gas: this.genGases(),
+					coordinates: [-4.45021390914917, 36.71963561245719]
+				},
+				{
+					gas: this.genGases(),
+					coordinates: [-4.431438446044922, 36.73240113505636]
+				},
+				{
+					gas: this.genGases(),
+					coordinates: [-4.478076696395874, 36.715215125584294]
+				},
+				{
+					gas: this.genGases(),
+					coordinates: [-4.490876197814941, 36.67908918792989]
+				},
+				{
+					gas: this.genGases(),
+					coordinates: [-4.486284255981445, 36.74129146113483]
+				},
+				{
+					gas: this.genGases(),
+					coordinates: [-4.415860176086426, 36.749613386425054]
+				}
+			];
+		},
+		methods: {
+			genGases() {
+				return {
+					co2: this.genGas(200, 2000),
+					o3: this.genGas(30, 70),
+					no2: this.genGas(20, 110),
+					so2: this.genGas(30, 220)
+				};
+			},
+			genGas(low, high) {
+				return Math.round(Math.random() * high + low);
+			},
+			avgGas(gas) {
+				return Math.round(
+					this.data.reduce((p, c) => p + c.gas[gas], 0) /
+						this.data.length
+				);
+			}
+		},
+		components: {
+			Navbar,
+			Map,
+			Stat
+		}
+	};
 </script>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,147 +1,157 @@
 <template>
-  <c-box w="100%" mb="4" p="4" shadow="sm">
-    <div id="map"></div>
-    <c-stat pl="4">
-      <c-stat-label>Connected devices</c-stat-label>
-      <c-stat-number>{{
-        points.length > 0
-          ? `${points.length} connected`
-          : "No devices connected"
-      }}</c-stat-number>
-    </c-stat>
-  </c-box>
+	<c-box w="100%" mb="4" p="4" shadow="sm">
+		<div id="map"></div>
+		<c-stat pl="4">
+			<c-stat-label>Connected devices</c-stat-label>
+			<c-stat-number>{{
+				points.length > 0
+					? `${points.length} connected`
+					: 'No devices connected'
+			}}</c-stat-number>
+		</c-stat>
+	</c-box>
 </template>
 
 <script>
-import { select } from "d3";
-import { geoAzimuthalEquidistant, geoPath } from "d3-geo";
-import { geoVoronoi } from "d3-geo-voronoi";
-import * as topojson from "topojson";
-import map from "../mapred.json";
+	import { select } from 'd3';
+	import { geoAzimuthalEquidistant, geoPath } from 'd3-geo';
+	import { geoVoronoi } from 'd3-geo-voronoi';
+	import * as topojson from 'topojson';
+	import map from '../mapred.json';
 
-export default {
-  name: "Map",
-  data: () => ({
-    width: 1920,
-    height: 600,
-    background: "#222",
-    pointSize: 2,
-    pointColor: "Black",
-    // muniFilter: [], [2, 3, 5, 8, 9, 10],
-  }),
-  watch: {
-    data: function () {
-      this.drawVoronoi();
-      this.drawPoints();
-    },
-  },
-  props: {
-    data: Array,
-  },
-  computed: {
-    points() {
-      return this.data.map(this.pointBuilder);
-    },
-  },
-  mounted() {
-    this.features = topojson.merge(map, map.objects.municipios.geometries);
+	export default {
+		name: 'Map',
+		data: () => ({
+			width: 1920,
+			height: 600,
+			background: '#222',
+			pointSize: 2,
+			pointColor: 'Black'
+			// muniFilter: [], [2, 3, 5, 8, 9, 10],
+		}),
+		watch: {
+			data: function() {
+				this.drawVoronoi();
+				this.drawPoints();
+			}
+		},
+		props: {
+			data: Array
+		},
+		computed: {
+			points() {
+				return this.data.map(this.pointBuilder);
+			}
+		},
+		mounted() {
+			this.features = topojson.merge(
+				map,
+				map.objects.municipios.geometries
+			);
 
-    this.mesh = topojson.mesh(map, map.objects.municipios, (a, b) => a !== b);
+			this.mesh = topojson.mesh(
+				map,
+				map.objects.municipios,
+				(a, b) => a !== b
+			);
 
-    this.svg = select("#map")
-      .append("svg")
-      .attr("viewBox", [0, 0, this.width, this.height])
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .classed("svg-content", true);
+			this.svg = select('#map')
+				.append('svg')
+				.attr('viewBox', [0, 0, this.width, this.height])
+				.attr('preserveAspectRatio', 'xMinYMin meet')
+				.classed('svg-content', true);
 
-    this.clip = this.svg
-      .append("defs")
-      .append("clipPath")
-      .attr("id", "clip-mask");
+			this.clip = this.svg
+				.append('defs')
+				.append('clipPath')
+				.attr('id', 'clip-mask');
 
-    this.projection = geoAzimuthalEquidistant().fitSize(
-      [this.width, this.height],
-      this.features
-    );
+			this.projection = geoAzimuthalEquidistant().fitSize(
+				[this.width, this.height],
+				this.features
+			);
 
-    this.path = geoPath().projection(this.projection);
+			this.path = geoPath().projection(this.projection);
 
-    this.clip.append("path").datum(this.features).attr("d", this.path);
+			this.clip
+				.append('path')
+				.datum(this.features)
+				.attr('d', this.path);
 
-    this.svg
-      .append("path")
-      .datum(this.features)
-      .attr("fill", this.background)
-      .attr("stroke", this.background)
-      .attr("stroke-width", 1)
-      .attr("d", this.path);
+			this.svg
+				.append('path')
+				.datum(this.features)
+				.attr('fill', this.background)
+				.attr('stroke', this.background)
+				.attr('stroke-width', 1)
+				.attr('d', this.path);
 
-    this.voronoiNode = this.svg
-      .append("g")
-      .attr("fill", "none")
-      .attr("stroke", this.background)
-      .attr("stroke-width", 0.5)
-      .attr("pointer-events", "all")
-      .attr("clip-path", "url(#clip-mask)");
+			this.voronoiNode = this.svg
+				.append('g')
+				.attr('fill', this.background)
+				.attr('stroke', this.background)
+				.attr('stroke-width', 0.5)
+				.attr('clip-path', 'url(#clip-mask)');
 
-    /*
-    this.svg
-      .append("path")
-      .datum(this.mesh)
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-linejoin", "miter")
-      .attr("stroke-width", "0.2")
-      .attr("d", this.path);
-      */
-  },
-  methods: {
-    pointBuilder({ gas, coordinates }) {
-      return {
-        type: "Feature",
-        properties: {
-          gas,
-        },
-        geometry: {
-          type: "Point",
-          coordinates,
-        },
-      };
-    },
-    drawPoints() {
-      if (this.pointPath) this.pointPath.remove();
-      this.pointPath = this.svg
-        .append("path")
-        .datum({
-          type: "FeatureCollection",
-          features: this.points,
-        })
-        .attr("d", this.path.pointRadius(this.pointSize))
-        .attr("fill", this.pointColor);
-    },
-    drawVoronoi() {
-      if (this.voronoiSubNode) this.voronoiSubNode.remove();
-      this.voronoiSubNode = this.voronoiNode
-        .append("g")
-        .selectAll("path")
-        .data(geoVoronoi().polygons(this.points).features)
-        .enter()
-        .append("path")
-        .attr("d", this.path)
-        .attr("class", "polygon")
-        .attr("fill", (p) => {
-          const g = p.properties.site.properties.gas.co2;
-          if (g < 1000) return "forestgreen";
-          else if (g < 2000) return "Gold";
-          else return "FireBrick";
-        });
-    },
-  },
-};
+			/*
+	    this.svg
+	      .append("path")
+	      .datum(this.mesh)
+	      .attr("fill", "none")
+	      .attr("stroke", "white")
+	      .attr("stroke-linejoin", "miter")
+	      .attr("stroke-width", "0.2")
+	      .attr("d", this.path);
+	      */
+		},
+		methods: {
+			pointBuilder({ gas, coordinates }) {
+				return {
+					type: 'Feature',
+					properties: {
+						gas
+					},
+					geometry: {
+						type: 'Point',
+						coordinates: coordinates.map((c) => c.toFixed(6))
+					}
+				};
+			},
+			drawPoints() {
+				if (this.pointPath) this.pointPath.remove();
+				this.pointPath = this.svg
+					.append('path')
+					.datum({
+						type: 'FeatureCollection',
+						features: this.points
+					})
+					.attr('d', this.path.pointRadius(this.pointSize))
+					.attr('fill', this.pointColor);
+			},
+			drawVoronoi() {
+				if (this.voronoiSubNode) this.voronoiSubNode.remove();
+				this.voronoiSubNode = this.voronoiNode
+					.append('g')
+					.selectAll('path')
+					.data(geoVoronoi().polygons(this.points).features)
+					.enter()
+					.append('path')
+					.attr('d', this.path)
+					.attr('stroke', this.background)
+					.attr('pointer-events', 'all')
+					.attr('fill', (p) => {
+						const g = p.properties.site.properties.gas.co2;
+						if (g < 1000) return 'forestgreen';
+						else if (g < 2000) return 'Gold';
+						else return 'FireBrick';
+					});
+
+				this.voronoiSubNode.append('title').text((node) => {
+					let gs = node.properties.site.properties.gas;
+					const cs = node.properties.sitecoordinates;
+					return `Lon: ${cs[0]}, Lat: ${cs[1]}\nCO2: ${gs.co2}\nNO2: ${gs.no2}\nO3:  ${gs.o3}\nSO2: ${gs.so2}`;
+				});
+			}
+		}
+	};
 </script>
-
-<style scoped>
-.polygon {
-  stroke: #00f;
-}
-</style>
